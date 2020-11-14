3eca00ee170b6e7c5a574c3212b9b2d923e3559c
Asterioned <71905174+Asterioned@users.noreply.github.com>
2020-11-15 00:49:03 +0100

    push

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ client.on('message', (message) => {
 client.on('ready', () => {
     console.log(`${client.user.tag} is Online and ready to go`)
     client.user.setUsername("2Cool4Skool")
-        bot.user.setStatus('available')
-        bot.user.setPresence({
+    client.user.setStatus('available')
+    client.user.setPresence({
             game: {
                 name: '-admin',
                 type: "PLAYING",
